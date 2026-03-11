1943dff6c95df1cc252861c9de93f1498628471b
Waleed-Zaheer <itzwaleedxd2@gmail.com>
2026-03-11 15:04:43 +0500

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="font-delius">
+        Click on the Vite and React logos to learn more
+      </p>
+      <p className="font-mono">
+        Click on the Vite and React logos to learn more
+      </p>
+      <p>Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
